9cad30a4b2482bbff469c999889456142c296bf9
YihaoCCC <yihao168830@163.com>
2021-12-22 22:25:15 +0800

    promise

---
+++ JsTest/promise.js
@@ -0,0 +1,54 @@
+//通过⼀个超⻓的链式调⽤我们学习⼀下链式调⽤的注意事项
+var p = new Promise(function(resolve,reject){
+    resolve('我是Promise的值')
+   })
+
+   console.log(p)
+
+   p.then(function(res){
+    //该res的结果是resolve传递的参数
+    console.log(res)
+   }).then(function(res){
+    //该res的结果是undefined
+    console.log(res)
+    return '123'
+   }).then(function(res){
+    //该res的结果是123
+    console.log(res)
+    return new Promise(function(resolve){
+    resolve(456)
+    })
+   }).then(function(res){
+    //该res的结果是456
+    console.log(res)
+    return '我是直接返回的结果'
+   }).then()
+    .then('我是字符串')
+    .then(function(res){
+    //该res的结果是“我是直接返回的结果”
+    console.log(res)
+   })
+
+   let p1 = new Promise((resolve,reject) => {
+    setTimeout(() => {
+    resolve('第⼀个promise执⾏完毕')
+    },1000)
+   })
+   let p2 = new Promise((resolve,reject) => {
+    setTimeout(() => {
+    resolve('第⼆个promise执⾏完毕')
+    },2000)
+   })
+   let p3 = new Promise((resolve,reject) => {
+    setTimeout(() => {
+    resolve('第三个promise执⾏完毕')
+    },3000)
+   })
+   Promise.all([p1,p3,p2]).then(res => {
+    console.log(res)
+   }).catch(function(err){
+    console.log(err)
+   })
+
+
+    
